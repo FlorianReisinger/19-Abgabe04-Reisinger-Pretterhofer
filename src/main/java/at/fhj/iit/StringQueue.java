34361bdaee2779872c756d546984375fc1fa5b49
Thomas Pretterhofer <thomas.Pretterhofer@gmail.com>
2019-06-09 13:39:08 +0200

    Added check to Construktor if maxSize <= 0. Readme.md updated.

--- src/main/java/at/fhj/iit/StringQueue.java
+++ src/main/java/at/fhj/iit/StringQueue.java
@@ -2,65 +2,69 @@ package at.fhj.iit;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.ArrayList;
 
 public class StringQueue implements Queue {
-	
-	private List<String> elements = new ArrayList<>();
-	private int maxSize = 5;
 
-	public StringQueue(int maxSize){
-		this.maxSize = maxSize;
-	}
+    private List<String> elements = new ArrayList<>();
+    private int maxSize = 5;
 
-import java.util.ArrayList;
-	@Override
-	public boolean offer(String obj) {
-		if(elements.size()!= maxSize)
-			elements.add(obj);
-		else
-			return false;
-		
-		return true;
-	}
+    public StringQueue(int maxSize) {
+        if (maxSize <= 0) {
+            throw new IllegalArgumentException("Size of StringQueue must be greater than 0!");
+        } else {
+            this.maxSize = maxSize;
+        }
+    }
 
-	@Override
-	public String poll() {
-		String element = peek();
-		
-		if(elements.size() > 0){
-			elements.remove(0);
-		}
-		
-		return element;
-	}
+    @Override
+    public boolean offer(String obj) {
+        if (elements.size() != maxSize)
+            elements.add(obj);
+        else
+            return false;
 
-	@Override
-	public String remove() {
-		String element = poll();
-		if(element == null)
-			throw new NoSuchElementException("there's no element any more");
-		
-		return element;
-	}
+        return true;
+    }
 
-	@Override
-	public String peek() {
-		String element;
-		if(elements.size() > 0)
-			element = elements.get(0);
-		else
-			element = null;
-		
-		return element;
-	}
+    @Override
+    public String poll() {
+        String element = peek();
 
-	@Override
-	public String element() {
-		String element = peek();
-		if(element == null)
-			throw new NoSuchElementException("there's no element any more");
-		
-		return element;
-	}
+        if (elements.size() > 0) {
+            elements.remove(0);
+        }
+
+        return element;
+    }
+
+    @Override
+    public String remove() {
+        String element = poll();
+        if (element == null)
+            throw new NoSuchElementException("there's no element any more");
+
+        return element;
+    }
+
+    @Override
+    public String peek() {
+        String element;
+        if (elements.size() > 0)
+            element = elements.get(0);
+        else
+            element = null;
+
+        return element;
+    }
+
+    @Override
+    public String element() {
+        String element = peek();
+        if (element == null)
+            throw new NoSuchElementException("there's no element any more");
+
+        return element;
+    }
 
 }
